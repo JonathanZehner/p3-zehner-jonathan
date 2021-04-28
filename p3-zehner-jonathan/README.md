3a787da8aa7e0570b5949702212c7ff399044ee5
Jonathan A. Zehner <78240659+JonathanZehner@users.noreply.github.com>
2021-04-27 21:24:22 -0400

    added files via upload to remote repository

---
+++ p3-zehner-jonathan/README.md
@@ -0,0 +1,28 @@
+# **assignment4-zehner-jonathan**
+
+## **Jonathan A. Zehner**
+
+Spring 2021 / MMC5277 Web Design Principles
+
+## **Resources used:**
+1. W3Schools.com
+2. Ducket, J. (2011). <em>HTML & CSS: design and build websites</em> John Wiley & Sons.
+3. Pleitez, J. (2021). <em>How to create animated birds on a web app with css!</em> 
+        https://medium.com/dev-genius/how-to-create-animated-birds-on-a-web-app-with-css-2415627acfa9
+4. Adobe Illustrator (AI) to create and modify the Brand IDs and logos throughout the site, and to create the bird animations.
+5. Adobe Photoshop to convert AI files to useable image file types.
+6. Adobe XD to create the wireframes and stylesheet used to build the website.
+
+
+**Note:**
+After running code through the HTML and CSS validators, deamed errors eroneous for proper display of website.
+
+I, *Jonathan A. Zehner*, have read the point deduction list and understand that I will lose points for missing items.
+
+
+
+
+
+
+## Designs by *Jonathan A. Zehner*
+![Designs by Jonathan A. Zehner](z-lettermark-rectangle-no-border.png)
---
+++ p3-zehner-jonathan/form/default.inc.php
@@ -0,0 +1,245 @@
+<style media="screen">
+* {
+  border: 0;
+  margin: 0;
+  padding: 0;
+}
+
+/*----------------------------------
+
+
+      Universal Styling
+
+
+-----------------------------------*/
+
+body {
+  background: #161717c7;
+  color: #161717;
+  font: normal 16px/1.5 'mr-eaves-xl-modern', 'opificio', sans-serif;
+}
+
+div {
+  background: #fffffff2 url(../images/celestial-hands-notext-background.png) repeat fixed center;
+  border: 5px solid #a8a096;
+  margin: 0 auto;
+  padding: 10px 20px;
+  position: absolute;
+  top: 40%;
+  width: 80%;
+}
+
+.small {
+  height: 50px;
+  object-fit: cover;
+  width: 50px;
+}
+
+.logo {
+  width: 250px;
+}
+
+a {
+  text-decoration: none;
+  color: #161717;
+}
+
+/*-- Unvisited Link --*/
+a:link {
+  color: #161717;
+}
+
+/*-- Visisted Link --*/
+a:visited {
+  color: #9783AA;
+  font-weight: bold;
+}
+
+/*-- Mouse Over Link --*/
+a:hover {
+  text-decoration: underline;
+  font-size: 1.3em;
+  transition: .2s;
+}
+
+/*-- Header Styling --*/
+
+header {
+  background: #d7bcb7;
+  padding: 15px 25px;
+  position: fixed;
+  width: 100%;
+  z-index: 999;
+}
+
+header img {
+  position: absolute;
+}
+
+nav ul li a{
+  list-style-type: none;
+  padding: 5px 15px;
+  transition: .2s
+}
+
+.main-menu {
+  position: relative;
+  text-align: center;
+  border: none;
+}
+
+.main-menu ul li {
+  display: inline-block;
+  padding: 20px;
+  margin: 10px;
+  text-transform: uppercase;
+}
+
+.top-right-menu {
+  min-width: 200px;
+  position: fixed;
+  right: 10px;
+  top: 15px;
+  border: none;
+}
+
+.top-right-menu ul li {
+  display: inline-block;
+  margin: 10px;
+}
+/*-- header end --*/
+
+
+/*-- footer begin --*/
+
+footer {
+  background: #d7bcb7;
+  position: relative;
+  height: 150px;
+  top: 80%;
+}
+
+footer .small {
+    position: absolute;
+    margin: 34px 50px;
+    background: white;
+    border: 2px solid #a8a096;
+    border-radius: 10px;
+    width: 75px;
+    height: 75px;
+}
+
+.resources,
+.about,
+.other-links {
+  position: relative;
+  float: left;
+  left: 10%;
+  display: block;
+  margin: 15px 50px;
+  padding: 10px 0;
+  padding-left: 95px;
+  border-left: 2px solid #16171778;
+  text-align: center;
+}
+
+.resources a,
+.about a,
+.other-links a {
+    display: block;
+    transition: .2s;
+}
+
+address {
+  line-height: 1.2;
+}
+
+.address {
+    position: relative;
+    text-align: left;
+    margin: 15px 25px;
+    z-index: 999;
+    float: right;
+    border-left: 2px solid #16171778;
+    padding-left: 50px;
+}
+
+address a {
+    color: blanchedalmond;
+}
+
+.nwd {
+    line-height: 1.2;
+    margin: 3px auto;
+}
+/*-- footer end --*/
+</style>
+
+<body>
+  <header>
+
+  <!--- Brand Logo --------->
+    <h1><img src="images/bird-navlogo-transparent.png" class="logo" alt="flyCOLLECTIVE logo" title="Robin's Nest"></h1>
+
+  <!--- Navigation Bar ----->
+    <nav class="nav-bar">
+      <section class="main-menu">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="boutique.html">Boutique</a></li>
+          <li><a href="health.html">Health</a></li>
+        </ul>
+      </section>
+
+      <section class="top-right-menu">
+        <ul>
+          <li><a href="#">Search</a></li>
+          <li><a href="#">Login</a></li>
+          <li><a href="#">Subscribe</a></li>
+        </ul>
+      </section>
+    </nav>
+  </header>
+
+  <div>
+    <h1>Sorry, something went wrong with your submission</h1>
+    <p>Please try again</p>
+  </div>
+
+  <footer>
+    <img src="images/flycollective-logo.png" class="small" alt="image of logo" title="flyCOLLECTIVE">
+
+    <section class="resources">
+      <h4>Resources</h4>
+      <a href="#">Robin's Nest</a>
+      <a href="#">Domestic Violence Link</a>
+      <a href="#">DV Link</a>
+    </section>
+
+    <section class="about">
+      <h4>About</h4>
+      <a href="#">About</a>
+      <a href="#">Products</a>
+      <a href="#">Health and Wellness</a>
+    </section>
+
+    <section class="other-links">
+      <h4>Other Links</h4>
+      <a href="#">Sip & Shop</a>
+      <a href="#">Facebook</a>
+      <a href="#">Instagram</a>
+    </section>
+
+    <section class="address">
+      <address>
+        <p><a href="mailto:kelle.zehner@gmail.com">Kelle Zehner</a></p>
+        <p>P.O. Box ####</p>
+        <p>Jacksonville, NC 28540</p>
+        <p><a href="tel:+17199306427">719.930.6427</a></p>
+      </address>
+
+      <p class="nwd">&copy; <strong>Created by<br><a href="www.nomadwebdesigns.com">NomadWebDesigns</a></strong></p>
+    </section>
+  </footer>
+</body>
